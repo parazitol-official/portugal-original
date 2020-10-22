0d9496cad262e8d5c6a4ef6ab6c1305731f02768
parazitol-official <73265222+parazitol-official@users.noreply.github.com>
2020-10-22 06:50:27 +0300

    1

---
+++ js/pt5.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        pt: {
+            name: 'Portugal',
+            code: 'pt',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'Por exemplo, +351 123456789 ',
+            nameHelper: 'Pedro Gustavo',
+            nameError: 'Nome obrigatório',
+            phoneError: 'Número especificado incorretamente',
+            countryError: 'País obrigatório',
+            isDefault: true
+        }
+    };
+})();
---
+++ js/translater.js
@@ -0,0 +1,254 @@
+var new_lang = {
+
+    "lt1": "GOLPE ESMAGADOR CONTRA PARASITAS",
+
+"lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normaliza o funcionamento do trato gastrointestinal.",
+
+"lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Mata quaisquer helmintos",
+
+"lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Proporciona a excreção de produtos residuais oriundos do metabolismo de parasitos",
+
+"lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Impede a reinfecção",
+
+"lt6": "OFERTA <br>ESPECIAL",
+
+"lt7": "até o fim da promoção restam",
+
+"lt8": "<div class=\"price_old\"></div>  Preço normal",
+
+"lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Preço promocional",
+
+"lt10": "Encomendar",
+
+"lt11": "DE ACORDO COM OS ÚLTIMOS DADOS DA ORGANIZAÇÃO MUNDIAL DA SAÚDE,<br> MAIS DE 1 BILHÃO DE PESSOAS ESTÃO INFECTADAS COM PARASITAS.",
+
+"lt12": "90% deles nem sequer sabem que se tornaram um ambiente de alimentação de parasitas.",
+
+"lt13": "VOCÊ TEM  CERTEZA DE QUE NAO ESTA INFECTADO TAMBEM ? VERIFIQUE EM SI  MESMO SE TENS SINAIS DE INFECÇÃO!",
+
+"lt14": "SINAIS DA HELMINTOSE",
+
+"lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Cansaço",
+
+"lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Dores de cabeça, incômodos físicos",
+
+"lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Inflamação das vias respiratórias superiores",
+
+"lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Irritabilidade, agressão",
+
+"lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Erupções cutâneas de várias formas",
+
+"lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Redução do peso corporal, mesmo tendo bom apetite",
+
+"lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Distúrbio do sono",
+
+"lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Alergias (olhos lacrimejantes, erupções cutâneas, coceira no nariz)",
+
+"lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Constipações frequentes, angina, nariz entupido",
+
+"lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Dor nas articulações e  NOS músculos",
+
+"lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Crescimento do gânglio linfático",
+
+"lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Desordens digestivas: inchaço, obstipação ou diarréia",
+
+"lt27": "VIAS DA INFECÇÃO POR HELMINTOS",
+
+"lt28": "Vegetais mal lavados, frutas, bagas.",
+
+"lt29": "Carnes e peixes mal fritos",
+
+"lt30": "Água ou solo contaminado",
+
+"lt31": "Contatos com  OS animais de estimação",
+
+"lt32": "Lugares públicos e transportes",
+
+"lt33": "Picadas de insetos",
+
+"lt34": "Restaurantes e fast food",
+
+"lt35": "COMECE O TRATAMENTO AGORA MESMO!",
+
+"lt36": "COMEÇAR",
+
+"lt37": "Os parasitas <br>que causam terríveis complicações:",
+
+"lt38": "CISTICERCOSE",
+
+"lt39": "Na maioria das vezes (mais de 60%) os cisticercos penetram no cérebro, menos frequentemente nos músculos esqueléticos e nos olhos. Vive entre 5 e 30 anos, causa <span>os tumores cancerosos.</span> ",
+
+"lt40": "ASCÁRIDAS",
+
+"lt41": "Habitam e propagam-se nos órgãos digestivos. Leva à formação de <span>úlceras</span>  e <span>grandes hemorragias</span> ",
+
+"lt42": "ANCYLOSTOMA",
+
+"lt43": "Esses parasitas danificam a membrana mucosa e comem o sangue que sai dos vasos feridos. Eles deixam feridas hemorrágicas, resultando em <span>anemia grave</span>  e <span>sepse.</span> ",
+
+"lt44": "ECHINOCOCCUS MULTILOCULARIS ",
+
+"lt45": "Causa necrose do tecido do órgão afetado. Além disso, os vasos sanguíneos são destruídos e <span>deixam de funcionar.</span> ",
+
+"lt46": "AÇÃO MULTIATIVA <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt47": "MATA",
+
+"lt48": "todas as invasões o mais rápido possível",
+
+"lt49": "EXCRETA ",
+
+"lt50": "produtos do metabolismo de parasitas",
+
+"lt51": "FACILITA",
+
+"lt52": "purificação do organismo de toxinas",
+
+"lt53": "RESTAURA",
+
+"lt54": "funcionamento do sistema imunitário, endócrino, digestivo, nervoso, e cardiovascular",
+
+"lt55": "PROTEGE",
+
+"lt56": "da reinfecção, forma um ambiente patogénico no corpo humano e aumenta a imunidade.",
+
+"lt57": "ENCOMENDAR",
+
+"lt58": "COMPOSIÇÃO NATURAL POTENTE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt59": "MORINGA",
+
+"lt60": "Sua única composição proporciona a limpeza do corpo de substâncias nocivas e metais pesados. Combate eficazmente os parasitas gastrointestinais, afetando o DNA de outras pessoas. Limpa o intestino e o trato biliar.",
+
+"lt61": "PAU-D’ARCO",
+
+"lt62": "A substância composta Lapaсhol, possuindo propriedades peculiares, contido na casca dessa planta, impede a propagação de patógenos e inibe a propagação e o desenvolvimento de parasitas intracelulares.",
+
+"lt63": "NOZ PRETA",
+
+"lt64": "Os elementos da noz preta perturbam o metabolismo dos vermes, causam paralisia dos tecidos musculares em parasitas, aumentam também a imunidade e as funções protetoras do organismo humano, e purificam o sangue das toxinas.",
+
+"lt65": "FOLHA DE OLIVEIRA",
+
+"lt66": "Contém oleuropeína, que tem efeitos antiparasitários e antifúngicos, reduz os danos oxidativos no sangue, fígado, rins e tecidos cerebrais.",
+
+"lt67": "RAIZ DE AÇAFRÃO (CURCUMA)",
+
+"lt68": "A rizoma do RAIZ DE AÇAFRÃO  contém cineol (eucaliptol),raiz de AÇAFRÃO   e vitamina P. Cineol normaliza o funcionamento dos órgãos digestivos e deprime as funções vitais dos vermes, curcumina tem efeito antiparasitário contra nematóides, vitamina P revitaliza os órgãos da hematopoiese e limpa o sangue.",
+
+"lt69": "ASAFETIDA",
+
+"lt70": "É conhecida por seus potentes efeitos bactericida e microbicida. Sua capacidade de remover as ténias parasitárias do trato gastrointestinal é bem conhecida.",
+
+"lt71": "QUAL<span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> É SUA VANTAGEM VERSUS OUTROS PRODUTOS?",
+
+"lt72": "MEDICAMENTOS",
+
+"lt74": "MEDICAMENTOS",
+
+"lt75": "Contem produtos químicos<br> perigosos  <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt76": "Impactam nos órgãos e sistemas internos<br> <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt77": "Proporcionam<br> um efeito nocivo na imunidade <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt78": "Prejudicam os órgãos gastrointestinais:<br> <span>fígado, estômago, pâncreas, intestinos, provocam<br> gastrite e úlceras.</span> ",
+
+"lt80": "Tem a composição totalmente<br> natural <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt81": "Sem efeitos secundários<br>, protege<br> os órgãos internos. <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt82": "Aumenta a imunidade<br> e a resistência do <br>organismo às infecções <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt83": "Proporciona um efeito benéfico no trato gastrointestinal:<br> <span>, melhora o fluxo biliar, estimula a função intestinal</span> ",
+
+"lt84": " <span>Parazitol</span> GENTIL PARA O HOMEM - DIFÍCIL PARA OS HELMINTOS",
+
+"lt85": "«A maioria da humanidade nem sabe que uma em duas pessoas é PARAZITADA.",
+
+"lt86": "Em 95% dos casos, a presença de helmintos não causa quaisquer sintomas, a menos que causem danos irreparáveis aos órgãos internos e perturbem o trabalho de todos os sistemas corporais.",
+
+"lt87": "Hoje em dia, apenas o fármaco Parazitol é capaz de limpar COMPLETAMENTE o corpo humano de mais de 250 diversas espécies de parasitas. Ele cria um ambiente no corpo humano que destrói o próprio processo de parasitização.",
+
+"lt88": "A experiência dos meus pacientes confirma que Parazitol é o fármaco mais eficaz e seguro para destruir e excretar TODOS os tipos de invasões, incluindo os seus ovos, assim impedindo a reinfecção do organismo.",
+
+"lt89": "Em 100% dos casos proporciona-se um efeito positivo».",
+
+"lt90": "Francisco Alves, médico parasitologista ",
+
+"lt91": "EFEITO LATERAL <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt92": "Ajuda a purificar órgãos e tecidos, neutraliza a intoxicação devido a uma infecção parasitária.",
+
+"lt93": "Regenera as membranas mucosas, limpa o sangue, linfa, fluidos intercelulares.",
+
+"lt94": "Normaliza o equilíbrio da oxidação e redução que pode causar o crescimento dos oncócitos",
+
+"lt95": "Normaliza o equilíbrio da oxidação e redução que pode causar o crescimento dos oncócitos",
+
+"lt96": "Facilita a cura de úlceras e gastrite matando as bactérias que as provocam.",
+
+"lt97": "Restaura e normaliza as funções dos órgãos prejudicados",
+
+"lt98": "Efeito benéfico sobre os órgãos gastrointestinais",
+
+"lt99": "Melhora a pele, o cabelo, as unhas.",
+
+"lt100": "ENCOMENDAR",
+
+"lt101": "FEEDBACK DE CLIENTES",
+
+"lt102": "<div class=\"slide-description-title\">Lívia Almeida, Sao Paulo</div> «Eu gostei muito deste produto. Meu marido e eu já tomamos a quantidade prescrita desse PRODUTO .  E ambos tinhamos as reações corporais diferentes: havia suor frio, diarreia, um pouco de borbulhas na pele e terrível dor de cabeça. Depois de uma semana e meia, essa fase aguda acabou e nós ambos estamos muito bem! O Parazitol funciona muito suavemente, mas de uma forma forte e confiante».",
+
+"lt103": "<div class=\"slide-description-title\">Sabrina, Alagoinhas</div> «Aconteceu assim que não me sentia bem durante duas semanas, fui testada, e a razão do mal-estar foi a infestação do meu corpo com parasitas. Tenho tomado comprimidos receitados pelo médico, mas tem sido pior a cada dia. Não sei o que me teria acontecido se não estivesse informada sobre novo fármaco Parazitol. Foi esse fármaco que me salvou. Quando eu fui testada dentro de um mês, nao havia nenhuns parasitas. Agora vou tomar o Parazitol uma vez por ano para prevenção».",
+
+"lt104": "<div class=\"slide-description-title\">Paulo, Goiânia</div> «O nosso gato não gosta de estar em casa, prefere passear no quintal. Foi assim que ficamos infestados com helmintas. Eu estava bem, mas a minha mulher tinha sofrido muito.  Primeiro ela rangeu os dentes à noite, depois começou a ter febre e a vomitar. Fomos tratados juntos porque provavelmente ela também me podia ter infestado. Tomamos o Parazitol como foi prescrito e afinal tudo ficou bem».",
+
+"lt105": "<div class=\"slide-description-title\">Lívia Almeida, Sao Paulo</div> «Eu gostei muito deste produto. Meu marido e eu já tomamos a quantidade prescrita desse fármaco.  E ambos tinhamos as reações corporais diferentes: havia suor frio, diarreia, um pouco de borbulhas na pele e terrível dor de cabeça. Depois de uma semana e meia, essa fase aguda acabou e nós ambos estamos muito bem! O Parazitol funciona muito suavemente, mas de uma forma forte e confiante».",
+
+"lt106": "<div class=\"slide-description-title\">Sabrina, Alagoinhas</div> «Aconteceu assim que não me sentia bem durante duas semanas, fui testada, e a razão do mal-estar foi a infestação do meu corpo com parasitas. Tenho tomado comprimidos receitados pelo médico, mas tem sido pior a cada dia. Não sei o que me teria acontecido se não estivesse informada sobre novo pRODUTO Parazitol. Foi esse fármaco que me salvou. Quando eu fui testada dentro de um mês, nao havia nenhuns parasitas. Agora vou tomar o Parazitol uma vez por ano para prevenção».",
+
+"lt107": "<div class=\"slide-description-title\">Paulo, Goiânia</div>  «O nosso gato não gosta de estar em casa, prefere passear no quintal. Foi assim que ficamos infestados com helmintas. Eu estava bem, mas a minha mulher tinha sofrido muito.  Primeiro ela rangeu os dentes à noite, depois começou a ter febre e a vomitar. Fomos tratados juntos porque provavelmente ela também me podia ter infestado. Tomamos o Parazitol como foi prescrito e afinal tudo ficou bem».",
+
+"lt108": "<div class=\"slide-description-title\">Lívia Almeida, Sao Paulo</div> «Eu gostei muito deste produto. Meu marido e eu já tomamos a quantidade prescrita desse fármaco.  E ambos tinhamos as reações corporais diferentes: havia suor frio, diarreia, um pouco de borbulhas na pele e terrível dor de cabeça. Depois de uma semana e meia, essa fase aguda acabou e nós ambos estamos muito bem! O Parazitol funciona muito suavemente, mas de uma forma forte e confiante».",
+
+"lt110": "GOLPE ESMAGADOR CONTRA PARASITAS",
+
+"lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normaliza o funcionamento do trato gastrointestinal.",
+
+"lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Mata quaisquer helmintos",
+
+"lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Proporciona a excreção de produtos residuais oriundos do metabolismo de parasitos",
+
+"lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Impede a reinfecção",
+
+"lt115": "OFERTA <br>ESPECIAL",
+
+"lt116": "até o fim da promoção restam",
+
+"lt117": "<div class=\"price_old\"></div>  Preço normal",
+
+"lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Preço promocional",
+
+"lt119": "Encomendar",
+
+"lt120": "Nosso horário de serviço",
+
+"lt121": "Faça o seu pedido no nosso site",
+
+"lt122": "Nosso operador entrará em contato com você para confirmar o pedido.",
+
+"lt123": "Você paga ao receber o produto encomendado"
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
